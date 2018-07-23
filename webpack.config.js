1d2f8d36e095eba5647d1d983617e244e496b39e
manishavi <manishalal972@gmail.com>
2018-07-23 16:38:14 -0500

    added image webpack loader

--- webpack.config.js
+++ webpack.config.js
@@ -1,24 +1,49 @@
 const path = require('path');
 
 module.exports = {
-  entry: './src/index.js',
+  entry: "./src/index.js",
   output: {
-    filename: 'bundle.js',
-    path: path.resolve(__dirname, 'dist')
+    filename: "bundle.js",
+    path: path.resolve(__dirname, "dist")
   },
   module: {
     rules: [
       {
         test: /\.css$/,
-        use: [
-          'style-loader',
-          'css-loader'
-        ]
+        use: ["style-loader", "css-loader"]
       },
       {
-        test: /\.(png|svg|jpg|gif)$/,
+        // test: /\.(png|svg|jpg|gif)$/,
+        // use: [
+        //   'file-loader'
+        // ]
+        test: /\.(gif|png|jpe?g|svg)$/i,
         use: [
-          'file-loader'
+          "file-loader",
+          {
+            loader: "image-webpack-loader",
+            options: {
+              mozjpeg: {
+                progressive: true,
+                quality: 65
+              },
+              // optipng.enabled: false will disable optipng
+              optipng: {
+                enabled: false
+              },
+              pngquant: {
+                quality: "65-90",
+                speed: 4
+              },
+              gifsicle: {
+                interlaced: false
+              },
+              // the webp option will enable WEBP
+              webp: {
+                quality: 75
+              }
+            }
+          }
         ]
       }
     ]
