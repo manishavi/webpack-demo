2e39c7f0a67b5e763678eca1c827b7bf500df5e4
manishavi <manishalal972@gmail.com>
2018-07-23 17:50:20 -0500

    added fonts to file loader

--- webpack.config.js
+++ webpack.config.js
@@ -13,37 +13,15 @@ module.exports = {
         use: ["style-loader", "css-loader"]
       },
       {
-        // test: /\.(png|svg|jpg|gif)$/,
-        // use: [
-        //   'file-loader'
-        // ]
-        test: /\.(gif|png|jpe?g|svg)$/i,
+        test: /\.(png|svg|jpg|gif)$/,
         use: [
-          "file-loader",
-          {
-            loader: "image-webpack-loader",
-            options: {
-              mozjpeg: {
-                progressive: true,
-                quality: 65
-              },
-              // optipng.enabled: false will disable optipng
-              optipng: {
-                enabled: false
-              },
-              pngquant: {
-                quality: "65-90",
-                speed: 4
-              },
-              gifsicle: {
-                interlaced: false
-              },
-              // the webp option will enable WEBP
-              webp: {
-                quality: 75
-              }
-            }
-          }
+          'file-loader'
+        ]
+      },
+      {
+        test: /\.(woff|woff2|eot|ttf|otf)$/,
+        use: [
+          'file-loader'
         ]
       }
     ]
