4408f7751a73334809aaeb6739d160d670aed2c6
manishavi <manishalal972@gmail.com>
2018-07-30 22:43:33 -0500

    use dynamic importing to separate a chunk

--- src/index.js
+++ src/index.js
@@ -1,38 +1,13 @@
-import _ from 'lodash';
-import printMe from './print';
-import { cube } from './math.js';
 
-if (process.env.NODE_ENV !== 'production') {
-  console.log('Looks like we are in development mode!');
+function getComponent() {
+  return import(/* webpackChunkName: "lodash" */ 'lodash').then(_ => {
+    var element = document.createElement('div');
+    var _ = _.default;
+    element.innerHTML = _.join(['Hello', 'webpack'], ' ');
+    return element;
+  }).catch(error => 'An error occurred while loading the component');
+
 }
-
-function component() {
-  // const element = document.createElement('div');
-  var element = document.createElement('pre');
-  const btn = document.createElement('button');
-
-  // element.innerHTML = _.join(['Hello', 'webpack'], ' ');
-  element.innerHTML = [
-    'Hello webpack!',
-    '5 cubed is equal to ' + cube(5)
-  ].join('\n\n');
-
-  btn.innerHTML = 'Click me and check the console';
-  btn.onclick = printMe;
-
-  element.appendChild(btn);
-
-  return element;
-}
-
-let element = component();
-document.body.appendChild(element);
-
-if(module.hot) {
-  module.hot.accept('./print.js', function() {
-    console.log('Accepting the updated printMe module!');
-    document.body.removeChild(element);
-    element = component();
-    document.body.appendChild(element);
-  })
-}
+getComponent().then(component => {
+  document.body.appendChild(component);
+})
