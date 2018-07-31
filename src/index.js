fa00e00dbf831245e35af31a6fb0d3d23e3fcbec
manishavi <manishalal972@gmail.com>
2018-07-30 22:52:57 -0500

    import returns promise, it can be used with async function

--- src/index.js
+++ src/index.js
@@ -1,13 +1,10 @@
 
-function getComponent() {
-  return import(/* webpackChunkName: "lodash" */ 'lodash').then(_ => {
+async function getComponent() {
     var element = document.createElement('div');
-    var _ = _.default;
+    const _ = await import(/* webpackChunkName: "lodash" */ 'lodash');
     element.innerHTML = _.join(['Hello', 'webpack'], ' ');
-    return element;
-  }).catch(error => 'An error occurred while loading the component');
-
+  return element;
 }
 getComponent().then(component => {
   document.body.appendChild(component);
-})
+});
